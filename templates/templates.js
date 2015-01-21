4e830ed30c48052f29fc6f42c0b441f3281f711a
Maximilian Klein <dasmaxl@gmail.com>
2015-01-21 23:48:35 +0100

    polymer basis

--- templates/templates.js
+++
@@ -1,8 +0,0 @@
-/**
- * Processes a html Element with the given ID
- */
-function Template(id){
-  var html = $("#"+id).html();
-  html = html.replace(/&lt;%/g, "<%").replace(/%&gt;/g, "%>");
-  return _.template(html);
-}
